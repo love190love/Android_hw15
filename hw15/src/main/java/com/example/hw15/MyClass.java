550a03a9908c58c76fc8cc5421c9d3e66ae1068f
love190love <love190love@yahoo.com.tw>
2019-11-08 15:45:32 +0800

    Initial commit

---
+++ hw15/src/main/java/com/example/hw15/MyClass.java
@@ -0,0 +1,20 @@
+package com.example.hw15;
+
+public class MyClass {
+    public static void main(String[] argv) {
+        java.util.Scanner scanner = new
+                java.util.Scanner(System.in);
+        System.out.println("請輸入現在小時:");
+        int h = scanner.nextInt();
+        System.out.println("請輸入現在分鐘:");
+        int m = scanner.nextInt();
+
+        {
+            if ((h == 7 && m >= 30) || (h <= 17)) {
+                System.out.println("目前不在學校");
+            } else {
+                System.out.println("目前在學校");
+            }
+        }
+    }
+}

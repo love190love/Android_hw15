33b3026d789f40342dddf34c46a10ccacc32cdb4
love190love <56149448+love190love@users.noreply.github.com>
2019-11-08 16:05:55 +0800

    Update MyClass.java

--- hw15/src/main/java/com/example/hw15/MyClass.java
+++ hw15/src/main/java/com/example/hw15/MyClass.java
@@ -4,17 +4,18 @@ public class MyClass {
     public static void main(String[] argv) {
         java.util.Scanner scanner = new
                 java.util.Scanner(System.in);
-        System.out.println("請輸入現在小時:");
+        System.out.println("現在小時:");
         int h = scanner.nextInt();
-        System.out.println("請輸入現在分鐘:");
+        System.out.println("現在分鐘:");
         int m = scanner.nextInt();
 
+        if ((h >= 7 && m >= 30) )
         {
-            if ((h == 7 && m >= 30) || (h <= 16)) {
-                System.out.println("目前不在學校");
-            } else {
-                System.out.println("目前在學校");
-            }
+            System.out.println("目前在學校");
+        }
+        else
+        {
+            System.out.println("目前不在學校");
         }
     }
 }
